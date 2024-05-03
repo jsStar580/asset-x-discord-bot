ab5390c330bf590df08bf23a77529d981d01cbb4
jsStar580 <jonathanman810@gmail.com>
2024-05-03 17:14:20 +0900

    add: first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.login(token);
