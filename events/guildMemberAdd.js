db0fd634a0968f088bfe204ab6c1346f7e571bd2
jsStar580 <jonathanman810@gmail.com>
2024-05-07 13:48:51 +0900

    add: CAPTCHA

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,14 +1,15 @@
 const { Events } = require("discord.js");
 
-module.exports = {
-  name: Events.GuildMemberAdd,
-  async execute(member) {
-    if (member.user.bot == true) {
-      member.kick({ reason: "Not allowed bots" });
-      console.log("A bot is kicked.");
+// module.exports = {
+//   name: Events.GuildMemberAdd,
+//   async execute(member) {
+//     // if (member.user.bot == true) {
+//     //   member.kick({ reason: "Not allowed bots" });
+//     //   console.log("A bot is kicked.");
 
-    } else {
-      console.log("A new user is joined.");
-    }
-  },
-};
+//     // } else {
+//     //   console.log("A new user is joined.");
+//     // }
+    
+//   },
+// };
--- index.js
+++ index.js
@@ -8,7 +8,31 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
   ],
+});
+
+const { Captcha } = require("discord.js-captcha");
+
+const captcha = new Captcha(client, {
+  // roleID: "1236960404576403466", //optional
+  channelID: "1237033785288167515", //optional
+  sendToTextChannel: true, //optional, defaults to false
+  // addRoleOnSuccess: true, //optional, defaults to true. whether you want the bot to add the role to the user if the captcha is solved
+  kickOnFailure: true, //optional, defaults to true. whether you want the bot to kick the user if the captcha is failed
+  caseSensitive: true, //optional, defaults to true. whether you want the captcha responses to be case-sensitive
+  attempts: 3, //optional, defaults to 1. number of attempts before captcha is considered to be failed
+  timeout: 60000, //optional, defaults to 60000. time the user has to solve the captcha on each attempt in milliseconds
+  showAttemptCount: true, //optional, defaults to true. whether to show the number of attempts left in embed footer
+  // customPromptEmbed: new EmbedBuilder(), //customise the embed that will be sent to the user when the captcha is requested
+  // customSuccessEmbed: new EmbedBuilder(), //customise the embed that will be sent to the user when the captcha is solved
+  // customFailureEmbed: new EmbedBuilder(), //customise the embed that will be sent to the user when they fail to solve the captcha
+});
+
+client.on("guildMemberAdd", async (member) => {
+  //in your bot application in the dev portal, make sure you have intents turned on!
+  captcha.present(member); //captcha is created by the package, and sent to the member
 });
 
 const eventsPath = path.join(__dirname, "events");
